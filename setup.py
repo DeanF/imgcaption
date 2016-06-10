91be6d1215fe98f364d4441f7b063c07220ecf39
Dean Fenster <dean.fenster@gmail.com>
2016-06-10 16:21:15 +0300

    Moved requirements into setup file

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup
 
 here = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(here, 'requirements.txt')) as fp:
-    requirements = fp.read().splitlines()
-
 setup(name='imgcaption',
-      version='0.1.1',
+      version='0.1.2',
       description="Library that gets image captions using "
                   "Microsoft's https://www.captionbot.ai/",
       url='https://github.com/DeanF/imgcaption',
       author='DeanF',
       packages=['imgcaption'],
-      install_requires=requirements,
+      install_requires=['requests', 'six'],
       zip_safe=False)
